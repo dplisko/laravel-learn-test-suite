2f1ca65081fde503b1dade5e511bea883abe3f8f
dplisko <denis.plisko@ekleft.com>
2014-03-22 02:46:34 +0300

    Создание модели товаров и отзывов

---
+++ app/models/Item.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Denis
+ * Date: 22.03.14
+ * Time: 2:24
+ */
+
+class Item extends Eloquent{
+
+} 
---
+++ app/models/Item/Review.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Denis
+ * Date: 22.03.14
+ * Time: 2:25
+ */
+
+namespace Item;
+
+
+class Review extends \Eloquent{
+
+} 
